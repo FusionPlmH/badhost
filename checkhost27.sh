e5fc0fc7be8b132079f998f2a707c9ee662931ef
Fusion <leemenhin@gmail.com>
2020-07-24 18:52:44 +0800

    Update checkhost27.sh

--- checkhost27.sh
+++ checkhost27.sh
@@ -5,6 +5,6 @@ cat split.27 | while read output;
 do
 NMAP=$(nmap -sn $output )
 if ! [[ $NMAP == *"Host is up"* || $NMAP == *"closed"* ]]; then
-echo "$output" >> deadhost28
+echo "$output" >> deadhost27
 fi
 done
